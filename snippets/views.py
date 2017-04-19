c874bbe837e14fadf61adde1ad97b128f0f1340b
manish_tixdo <balyan05.manish@gmail.com>
2017-04-19 15:53:55 +0530

    part 2 completed

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,53 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from models import Snippet
+from serializers import SnippetSerializer
 from django.shortcuts import render
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
+
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets,many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['GET', 'POST'])
+def snippet_detail(request, pk, format=None):
+
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+
+    except Snippet.DoesNOtExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method =='GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
