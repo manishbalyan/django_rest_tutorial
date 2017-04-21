30ac857d83869a12b75891e73d4f9bd20217763e
manish_tixdo <balyan05.manish@gmail.com>
2017-04-21 17:22:05 +0530

    part 4 doing

--- snippets/views.py
+++ snippets/views.py
@@ -5,11 +5,13 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from models import Snippet
-from serializers import SnippetSerializer
+from serializers import SnippetSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
+
 
 
 
@@ -131,3 +133,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
